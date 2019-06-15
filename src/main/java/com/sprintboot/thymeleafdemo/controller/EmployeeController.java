8591bff3874ac6e7f8a1c40b3a400ddf9f3b041f
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-15 20:04:58 +0100

    Initial commit

---
+++ src/main/java/com/sprintboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.sprintboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sprintboot.thymeleafdemo.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	// LOAD EMPLOYEE DATA
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		//CREATE EMPLOYEES
+		Employee tempEmployee1 = new Employee(1, "Rodolfo", "Carvajal", "rodolfo@rodolfo.com");
+		Employee tempEmployee2 = new Employee(2, "Juan", "Velasquez", "juan@juan.com");
+		Employee tempEmployee3 = new Employee(3, "Cesar", "Padron", "cesar@cesar.com");
+		
+		//CREATE LIST
+		theEmployees = new ArrayList<>();
+				
+		//ADD TO THE LIST
+		theEmployees.add(tempEmployee1);
+		theEmployees.add(tempEmployee2);
+		theEmployees.add(tempEmployee3);
+		
+	}
+	
+	// ADD MAPPING "/LIST"
+	@GetMapping("/list")
+	public String listEmployees (Model theModel) {
+		
+		// ADD TO THE SPRING MODEL
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+}
