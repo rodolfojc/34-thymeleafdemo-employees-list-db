da2aa02e13bb20d5e0a70bdce2b19014a0c66732
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-17 20:00:23 +0100

    Update
    Demo

--- src/main/java/com/sprintboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/sprintboot/thymeleafdemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sprintboot.thymeleafdemo.entity.Employee;
 import com.sprintboot.thymeleafdemo.service.EmployeeService;
@@ -45,6 +46,21 @@ public class EmployeeController {
 		return "employees/employee-form";
 		
 	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate (@RequestParam("employeeId") int theId, Model theModel) {
+		
+		// GET THE EMPLOYEE FROM SERVICE
+		Employee tempEmployee = this.employeeService.findById(theId);
+			
+		// SET EMPLOYEE AS A MODEL ATTRIBUTE TO PRE-POPULATE THE FORM
+		theModel.addAttribute("employee", tempEmployee);
+		
+		// SEND OVER TO FORM
+		return "employees/employee-form";
+		
+	}
+	
 	
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
